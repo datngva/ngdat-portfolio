a6d23773faec7615fcfa3e607208aabc547649fc
Dat Nguyen <dat.ngva@gmail.com>
2025-07-08 22:06:37 +0700

    Update tonight

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = "Đạt Nguyễn";
+export const SITE_TITLE = "dat nguyen";
 export const SITE_DESCRIPTION =
-  "Xin chào, tôi là Đạt Nguyễn. Mục tiêu trở thành nhà thiết kế UX, nhà thiết kế sản phẩm, làm chủ quy trình UX và giải quyết các vấn đề của khách hàng và người dùng. ";
+  "Aspiring to be a UX Designer and Product Designer, mastering the UX process and solving customer and user problems. ";
